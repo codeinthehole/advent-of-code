dfa62a366ca9068f9c6b421cbdf21092a91aa313
David Winterbottom <david.winterbottom@gmail.com>
2022-12-02 19:14:19 +0000

    Adapt day 2 answer to use an `enum`
    Which makes it hilariously over-engineered.

--- 2022_02.py
+++ 2022_02.py
@@ -1,3 +1,4 @@
+import enum
 from typing import Callable, Iterator
 
 import aocd
@@ -22,6 +23,7 @@ def part2(lines: list[str]) -> int:
     A = rock
     B = paper
     C = scissors
+
     X = lose
     Y = draw
     Z = win
@@ -29,12 +31,18 @@ def part2(lines: list[str]) -> int:
     return sum(_line_scores(lines, choice_fn=_result_choices))
 
 
+class Choice(enum.Enum):
+    ROCK = enum.auto()
+    PAPER = enum.auto()
+    SCISSORS = enum.auto()
+
+
 # Helpers
 
 
-def _player_choices(lines: list[str]) -> Iterator[tuple[str, str]]:
+def _player_choices(lines: list[str]) -> Iterator[tuple[Choice, Choice]]:
     """
-    Convert the lines to an iterator of player moves where:
+    Convert the data lines to an iterator of player moves where:
 
         A = X = rock
         B = Y = paper
@@ -44,10 +52,21 @@ def _player_choices(lines: list[str]) -> Iterator[tuple[str, str]]:
     """
     for line in lines:
         parts = line.split()
-        yield parts[0], parts[1]
+        p1 = {
+            "A": Choice.ROCK,
+            "B": Choice.PAPER,
+            "C": Choice.SCISSORS,
+        }[parts[0]]
+        p2 = {
+            "X": Choice.ROCK,
+            "Y": Choice.PAPER,
+            "Z": Choice.SCISSORS,
+        }[parts[1]]
+
+        yield p1, p2
 
 
-def _result_choices(lines: list[str]) -> Iterator[tuple[str, str]]:
+def _result_choices(lines: list[str]) -> Iterator[tuple[Choice, Choice]]:
     """
     Convert the lines to an iterator of player moves where:
 
@@ -59,35 +78,37 @@ def _result_choices(lines: list[str]) -> Iterator[tuple[str, str]]:
     """
     for line in lines:
         parts = line.split()
-        p1, result = parts[0], parts[1]
-        if result == "X":
-            # Need to pick losing response
-            p2 = _losing_move(p1)
-        elif result == "Y":
-            # Need to draw
-            p2 = _drawing_move(p1)
-        elif result == "Z":
-            # Need to win
-            p2 = _winning_move(p1)
+        p1 = {
+            "A": Choice.ROCK,
+            "B": Choice.PAPER,
+            "C": Choice.SCISSORS,
+        }[parts[0]]
+
+        # Map the second column to a function that picks the right move.
+        p2 = {"X": _losing_move, "Y": _drawing_move, "Z": _winning_move}[parts[1]](p1)
+
         yield p1, p2
 
 
 def _line_scores(
-    lines: list[str], choice_fn: Callable[[list[str]], Iterator[tuple[str, str]]]
+    lines: list[str], choice_fn: Callable[[list[str]], Iterator[tuple[Choice, Choice]]]
 ) -> Iterator[int]:
+    """
+    Return an iterator of scores for each line.
+    """
     for p1, p2 in choice_fn(lines):
         yield _choice_score(p2) + _result_score(p1, p2)
 
 
-def _choice_score(choice: str) -> int:
+def _choice_score(choice: Choice) -> int:
     return {
-        "X": 1,
-        "Y": 2,
-        "Z": 3,
+        Choice.ROCK: 1,
+        Choice.PAPER: 2,
+        Choice.SCISSORS: 3,
     }[choice]
 
 
-def _result_score(p1: str, p2: str) -> int:
+def _result_score(p1: Choice, p2: Choice) -> int:
     if _is_p2_win(p1, p2):
         return 6
     elif _is_draw(p1, p2):
@@ -96,44 +117,32 @@ def _result_score(p1: str, p2: str) -> int:
         return 0
 
 
-def _losing_move(p1: str) -> str:
-    return {
-        "A": "Z",
-        "B": "X",
-        "C": "Y",
-    }[p1]
+# Map each choice to a tuple of (beats, loses).
+RESULT_MATRIX = {
+    Choice.ROCK: (Choice.SCISSORS, Choice.PAPER),
+    Choice.PAPER: (Choice.ROCK, Choice.SCISSORS),
+    Choice.SCISSORS: (Choice.PAPER, Choice.ROCK),
+}
 
 
-def _drawing_move(p1: str) -> str:
-    return {
-        "A": "X",
-        "B": "Y",
-        "C": "Z",
-    }[p1]
+def _is_p2_win(p1: Choice, p2: Choice) -> bool:
+    return p2 == _winning_move(p1)
 
 
-def _winning_move(p1: str) -> str:
-    return {
-        "A": "Y",
-        "B": "Z",
-        "C": "X",
-    }[p1]
+def _is_draw(p1: Choice, p2: Choice) -> bool:
+    return p1 == p2
 
 
-def _is_p2_win(p1: str, p2: str) -> bool:
-    return {
-        "A": "Y",
-        "B": "Z",
-        "C": "X",
-    }[p1] == p2
+def _losing_move(p1: Choice) -> Choice:
+    return RESULT_MATRIX[p1][0]
 
 
-def _is_draw(p1: str, p2: str) -> bool:
-    return {
-        "A": "X",
-        "B": "Y",
-        "C": "Z",
-    }[p1] == p2
+def _drawing_move(p1: Choice) -> Choice:
+    return p1
+
+
+def _winning_move(p1: Choice) -> Choice:
+    return RESULT_MATRIX[p1][1]
 
 
 EXAMPLE_DATA = [
@@ -146,6 +155,20 @@ EXAMPLE_DATA = [
 class TestPart1:
     def test_example_data(self):
         assert part1(EXAMPLE_DATA) == 15
+
+
+class TestChoiceScore:
+    def test_scores_are_correct(self):
+        assert _choice_score(Choice.ROCK) == 1
+        assert _choice_score(Choice.PAPER) == 2
+        assert _choice_score(Choice.SCISSORS) == 3
+
+
+class TestResultScore:
+    def test_winning_results(self):
+        assert _result_score(Choice.ROCK, Choice.PAPER) == 6
+        assert _result_score(Choice.PAPER, Choice.SCISSORS) == 6
+        assert _result_score(Choice.SCISSORS, Choice.ROCK) == 6
 
 
 class TestPart2:
